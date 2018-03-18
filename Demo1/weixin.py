05cb4df5d1d0512fca76421be459807ec44244b2
wangshumin <wangshumin@dianwoba.com>
2018-03-18 17:27:24 +0800

    11

--- Demo1/weixin.py
+++ Demo1/weixin.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hello world'
+    return 'hello worldaaaaaaaaaaaaa'
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=80)
